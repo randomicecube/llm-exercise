5e686bc8584edc27f3b20ab5509cafeef305ef2c
Diogo Gaspar <dgaspar@kth.se>
2023-09-14 19:36:06 +0200

    status update

--- flake.nix
+++ flake.nix
@@ -1,24 +1,22 @@
 {
   inputs = {
-    nixpkgs.url = "flake:nixpkgs";
-    mach-nix.url = "flake:mach-nix";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, mach-nix }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      machNix = mach-nix.lib."${system}";
-      myPython = machNix.mkPython {
-        requirements = ''
-          transformers
-        '';
-      };
-    in {
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [
-          myPython
-        ];
-      };
-    };
+  outputs = { nixpkgs-unstable, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs-unstable { inherit system; };
+        pythonPackages = p: with p; [ transformers torch accelerate einops huggingface-hub ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (python3.withPackages pythonPackages)
+          ];
+        };
+      }
+    );
 }
